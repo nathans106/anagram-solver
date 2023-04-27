d9ea6f124ce97061a6fd9b63a5159e1cebabbd97
Nathan Samra <n.samra@siemens.com>
2023-04-27 15:27:00 +0100

    Working

--- solve_anagram.py
+++ solve_anagram.py
@@ -1,8 +1,20 @@
 from argparse import ArgumentParser
+from itertools import permutations
+from pathlib import Path
+from typing import List
 
 parser = ArgumentParser("anagram-solver")
 parser.add_argument("letters")
 args = parser.parse_args()
 
-letters : List[str] = args.letters.split()
+letters : List[str] = list(args.letters)
+
+words_file = Path(__file__).parent / "google-10000-english" / "google-10000-english.txt"
+words = set(words_file.read_text().split("\n"))
+
+permutations = ["".join(letter_combo) for letter_combo in permutations(letters, len(letters))]
+
+for possible_word in permutations:
+    if possible_word in words:
+        print(possible_word)
 
